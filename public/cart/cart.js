79fc628d6bb2e732fec96c190389438431af76c7
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-06-15 20:35:48 +0530

    adding basic files

---
+++ public/cart/cart.js
@@ -0,0 +1,15 @@
+'use strict'
+angular.module('cart',['ngRoute'])
+
+ 
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/cart', {
+    templateUrl: 'public/cart/cart.html',
+    controller: 'CartCtrl'
+  });
+}])
+
+.controller('CartCtrl',[function(){
+
+
+}]);
